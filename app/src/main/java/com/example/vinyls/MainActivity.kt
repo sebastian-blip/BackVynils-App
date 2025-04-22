d21deb1d85f346f45719d5ad1439575fce5e7e49
sebastian Betancourth Zapata <s.betancourth@uniandes.edu.co>
2025-04-22 18:47:07 -0500

    se agrega la vista completa para crear album (#43)
    * se agrega la vista completa para crear album
    
    * Se modifica la estructura para que cumpla el patron y este todo con compose
    
    * se aplica toda la arquitectura para la pantalla de guardar album
    
    ---------
    
    Co-authored-by: sebastian-blip <s.betanccourth@uniandes.edu.co>

--- app/src/main/java/com/example/vinyls/MainActivity.kt
+++ app/src/main/java/com/example/vinyls/MainActivity.kt
@@ -5,13 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.vinyls.ui.crearalbum.CrearAlbumScreen
 import com.example.vinyls.ui.theme.VinylsTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +17,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VinylsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    CrearAlbumScreen()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VinylsTheme {
-        Greeting("Android")
-    }
 }
