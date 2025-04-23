ca003c0165d71806b5fe9c9e4ac7a4e0fa828223
ofmunozm <of.munoz@uniandes.edu.co>
2025-04-22 22:30:40 -0500

    conectar CrearPremioScreen con API usando MVVM y Volley

--- app/src/main/java/com/example/vinyls/ui/crearpremio/CrearPremioViewModel.kt
+++ app/src/main/java/com/example/vinyls/ui/crearpremio/CrearPremioViewModel.kt
@@ -1,16 +1,41 @@
 package com.example.vinyls.ui.crearpremio
 
-import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.vinyls.repositories.PremioRepository
 
-class CrearPremioViewModel : ViewModel() {
+class CrearPremioViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val premioRepository = PremioRepository(application)
+
     var nombre by mutableStateOf("")
     var descripcion by mutableStateOf("")
     var organizacion by mutableStateOf("")
 
-    fun crearPremio() {
-        // logica para api
+    fun crearPremio(
+        onSuccess: () -> Unit = {},
+        onError: (Exception) -> Unit = {}
+    ) {
+        if (nombre.isBlank() || descripcion.isBlank() || organizacion.isBlank()) {
+            onError(Exception("Todos los campos son obligatorios"))
+            return
+        }
+
+        premioRepository.crearPremio(
+            nombre,
+            descripcion,
+            organizacion,
+            onSuccess = {
+                println("Premio creado exitosamente")
+                onSuccess()
+            },
+            onError = {
+                println("Error al crear el premio: ${it.message}")
+                onError(it)
+            }
+        )
     }
 }
