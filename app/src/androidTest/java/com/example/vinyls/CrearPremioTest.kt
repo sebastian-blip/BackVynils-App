8bda625be5bf060da57fa979b8475f3c0f9dbec2
sebastian Betancourth Zapata <s.betancourth@uniandes.edu.co>
2025-04-27 23:00:50 -0500

    Feature/crear premio test e2 e (#64)
    * HU013 - Crearpremios-Implementa-formulario-de creación
    
    * HU013 - Crearpremios-Implementa-formulario-de creación
    
    * HU013 - se comenta el componente en el mainactivity y se verifica funcionamiento
    
    * conectar CrearPremioScreen con API usando MVVM y Volley
    
    * avance prueba E2E para crear premio.
    
    * borrar archivos que no son necesarios
    
    * borrar archivos que no son necesarios
    
    * Se Agregan los test de crear premios
    
    ---------
    
    Co-authored-by: ofmunozm <of.munoz@uniandes.edu.co>
    Co-authored-by: sebastian-blip <s.betanccourth@uniandes.edu.co>

--- app/src/androidTest/java/com/example/vinyls/CrearPremioTest.kt
+++ app/src/androidTest/java/com/example/vinyls/CrearPremioTest.kt
@@ -16,6 +16,18 @@ class CrearPremioScreenTest {
 
     @Test
     fun testCrearPremioCamposYBoton() {
+        composeTestRule.onNodeWithText("Continue").performClick()
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
+
+        composeTestRule.onNodeWithContentDescription("addPremioButton").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 10_000) {
+            composeTestRule.onAllNodesWithText("Crear Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
         composeTestRule.onNodeWithText("Nombre").performTextInput("Latin Grammy Awards")
         composeTestRule.onNodeWithText("Descripción").performTextInput("Premios otorgados por la Academia Latina de la Grabación para reconocer la excelencia en la música latina.")
         composeTestRule.onNodeWithText("Organización").performTextInput("Latin Recording Academy")
@@ -29,4 +41,137 @@ class CrearPremioScreenTest {
         composeTestRule.onNodeWithText("🎉 Premio creado exitosamente")
             .assertExists()
     }
+    @Test
+    fun testCrearPremioNombreVacio() {
+        composeTestRule.onNodeWithText("Continue").performClick()
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithContentDescription("addPremioButton").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 10_000) {
+            composeTestRule.onAllNodesWithText("Crear Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("Descripción").performTextInput("Premios otorgados por la Academia Latina de la Grabación para reconocer la excelencia en la música latina.")
+        composeTestRule.onNodeWithText("Organización").performTextInput("Latin Recording Academy")
+
+        composeTestRule.onNodeWithText("Crear").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("El nombre no puede estar vacío").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("El nombre no puede estar vacío")
+            .assertExists()
+    }
+
+    @Test
+    fun testCrearPremioDescripcionVacio() {
+        composeTestRule.onNodeWithText("Continue").performClick()
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithContentDescription("addPremioButton").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 10_000) {
+            composeTestRule.onAllNodesWithText("Crear Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("Nombre").performTextInput("Latin Grammy Awards")
+        composeTestRule.onNodeWithText("Organización").performTextInput("Latin Recording Academy")
+
+        composeTestRule.onNodeWithText("Crear").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("La descripción no puede estar vacía").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("La descripción no puede estar vacía")
+            .assertExists()
+    }
+
+
+    @Test
+    fun testCrearPremioorganizacionVacio() {
+        composeTestRule.onNodeWithText("Continue").performClick()
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithContentDescription("addPremioButton").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 10_000) {
+            composeTestRule.onAllNodesWithText("Crear Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("Nombre").performTextInput("Latin Grammy Awards")
+        composeTestRule.onNodeWithText("Descripción").performTextInput("Premios otorgados por la Academia Latina de la Grabación para reconocer la excelencia en la música latina.")
+
+        composeTestRule.onNodeWithText("Crear").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("La organización no puede estar vacía").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("La organización no puede estar vacía")
+            .assertExists()
+    }
+
+    @Test
+    fun testCrearPremioCamposEspeciales() {
+        composeTestRule.onNodeWithText("Continue").performClick()
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithContentDescription("addPremioButton").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 10_000) {
+            composeTestRule.onAllNodesWithText("Crear Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("Nombre").performTextInput("Latin Grammy Awards")
+        composeTestRule.onNodeWithText("Descripción").performTextInput("Premios otorgados$ por %la Academia Latina de la Grabación para reconocer la excelencia en la música latina.")
+        composeTestRule.onNodeWithText("Organización").performTextInput("$$%$%$")
+
+        composeTestRule.onNodeWithText("Crear").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("🎉 Premio creado exitosamente").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("🎉 Premio creado exitosamente")
+            .assertExists()
+    }
+
+    @Test
+    fun testCrearPremioStrinVacio() {
+        composeTestRule.onNodeWithText("Continue").performClick()
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithContentDescription("addPremioButton").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 10_000) {
+            composeTestRule.onAllNodesWithText("Crear Premios").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("Nombre").performTextInput(" ")
+        composeTestRule.onNodeWithText("Descripción").performTextInput(" ")
+        composeTestRule.onNodeWithText("Organización").performTextInput("  ")
+
+        composeTestRule.onNodeWithText("Crear").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("La organización no puede estar vacía").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("La organización no puede estar vacía")
+            .assertExists()
+    }
+
 }
