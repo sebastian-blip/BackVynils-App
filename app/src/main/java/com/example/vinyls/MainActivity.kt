ed31d5c7a662c26d97e1733247afdaa07b06c008
sebastian-blip <s.betanccourth@uniandes.edu.co>
2025-05-10 10:22:06 -0500

    se corriguen conflictos

--- app/src/main/java/com/example/vinyls/MainActivity.kt
+++ app/src/main/java/com/example/vinyls/MainActivity.kt
@@ -16,6 +16,8 @@ import com.example.vinyls.ui.crearpremio.CrearPremioScreen
 import com.example.vinyls.ui.homeapp.HomeAppScreen
 import com.example.vinyls.ui.login.LoginScreen
 import com.example.vinyls.ui.theme.VinylsTheme
+import com.example.vinyls.ui.detalleartista.DetalleArtistaScreen
+import com.example.vinyls.ui.albumlist.AlbumListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,6 +46,13 @@ class MainActivity : ComponentActivity() {
                         }
                         composable("crear_premios") {
                             CrearPremioScreen(navController)
+                        }
+                        composable("detalle_artista/{id}") { backStackEntry ->
+                            val id = backStackEntry.arguments?.getString("id")?.toIntOrNull() ?: 100
+                            DetalleArtistaScreen(navController = navController, artistaId = id)
+                        }
+                        composable("listar_albumns") {
+                            AlbumListScreen(navController = navController)
                         }
                     }
                 }
