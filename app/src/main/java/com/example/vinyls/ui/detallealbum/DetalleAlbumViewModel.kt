a6707d0ed0988b80e9ab9d653f0ae131dab63083
alejandra.pinzon <ma.pinzons1@uniandes.edu.co>
2025-05-10 22:37:53 -0500

    Integración con API / manejo de caché

--- app/src/main/java/com/example/vinyls/ui/detallealbum/DetalleAlbumViewModel.kt
+++ app/src/main/java/com/example/vinyls/ui/detallealbum/DetalleAlbumViewModel.kt
@@ -1,4 +1,35 @@
 package com.example.vinyls.ui.detallealbum
 
-class DetalleAlbumViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.vinyls.repositories.Album
+import com.example.vinyls.repositories.AlbumListRepository
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+
+class DetalleAlbumViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val albumRepository = AlbumListRepository(application)
+
+    private val _album = MutableStateFlow<Album?>(null)
+    val album: StateFlow<Album?> = _album
+
+    private var error by mutableStateOf<String?>(null)
+
+    // Cargar un álbum por su ID
+    fun loadAlbumById(albumId: Int) {
+        viewModelScope.launch {
+            try {
+                _album.value = albumRepository.getAlbumById(albumId)
+            } catch (e: Exception) {
+                error = e.message
+            }
+        }
+    }
 }
